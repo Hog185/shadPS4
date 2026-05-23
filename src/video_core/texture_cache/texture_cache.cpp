1c603f67976345cdd23d9e3aef0ea0ffb8a36a33
Hog <hogman12333@gmail.com>
2026-05-23 12:07:36 +0100

    Fix condition for expanding cached image size

--- src/video_core/texture_cache/texture_cache.cpp
+++ src/video_core/texture_cache/texture_cache.cpp
@@ -335,7 +335,7 @@ std::tuple<ImageId, int, int> TextureCache::ResolveOverlap(const ImageInfo& imag
 
         // Size and resources are greater, expand the image.
         if (image_info.type == cache_image.info.type &&
-            image_info.resources > cache_image.info.resources) {
+            image_info.guest_size > cache_image.info.guest_size) {
             return {ExpandImage(image_info, cache_image_id), -1, -1};
         }
 
